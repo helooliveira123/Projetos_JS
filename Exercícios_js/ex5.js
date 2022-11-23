624a54a13e37a89673e1906a18846116e1c4e1fd
Heloisa Oliveira <101333166+helooliveira123@users.noreply.github.com>
2022-11-23 16:18:50 -0300

    Delete Exercícios_js directory

--- Exercícios_js/ex5.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    let v1=parseFloat(document.getElementById('v1').value);
-    let v2=parseFloat(document.getElementById('v2').value);
-    let res=document.getElementById('res');
-
-    let m2=v1*v2;
-    let total=m2*36.00;
-    res.innerHTML="O preço total é R$" + total; 
-}
